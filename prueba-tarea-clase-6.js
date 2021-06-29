8d5f15c5845ecc1fa5aea33944177754e980e18e
Nicolas Misseri <nicomisseri1@gmail.com>
2021-06-29 09:32:57 -0300

    primer commit

---
+++ prueba-tarea-clase-6.js
@@ -0,0 +1,32 @@
+function probarValidarEdad() {
+  console.assert(
+    validarEdad("") === "El campo edad no puede estar vacio",
+    "validarEdad no valido que el campo no este vacio"
+  );
+
+  console.assert(
+    validarEdad("1.5") === "El campo edad debe ser un numero entero",
+    "validarEdad no valido que el numero sea entero"
+  );
+
+  console.assert(validarEdad(23) === "", "");
+}
+
+function probarValidarSalario() {
+  console.assert(
+    validarSalario("") === "El campo salario no pude estar vacio",
+    "validarSalario no valido que el campo no este vacio"
+  );
+  console.assert(
+    validarSalario("1.5") === "El campo salario debe ser un numero entero",
+    "validarSalario no valido que el numero sea entero"
+  );
+  console.assert(validarSalario(23) === "", "");
+}
+
+function hacerPruebas() {
+  probarValidarEdad();
+  probarValidarSalario();
+}
+
+hacerPruebas();
---
+++ tarea-clase-6.js
@@ -0,0 +1,230 @@
+/*
+TAREA: Empezar preguntando cuánta gente hay en el grupo familiar.
+Crear tantos inputs+labels como gente haya para completar la edad de cada integrante.
+Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad, la menor edad y el promedio del grupo familiar.
+
+Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, borrando los inputs ya creados (investigar cómo en MDN).
+*/
+
+// Preguntar cantidad de integrantes
+const cantidadDeIntegrantes = Number(
+  prompt("Cantidad de integrantes del grupo Familiar: ")
+);
+
+// VALIDACIONES
+
+function validarEdad(edad) {
+  if (edad.value === "") {
+    edad.className = "error";
+    return "El campo edad no puede estar vacio";
+  } else if (/[0-9]+$/.test(Number(edad.value))) {
+    edad.className = "error";
+    return "El campo edad debe ser un numero entero";
+  } else {
+    return "";
+  }
+}
+
+function validarSalario(salario) {
+  if (salario.value === "") {
+    salario.className = "error";
+    return "El campo salario no pude estar vacio";
+  } else if (/[0-9]+$/.test(Number(salario.value))) {
+    salario.className = "error";
+    return "El campo salario debe ser un numero entero";
+  } else {
+    return "";
+  }
+}
+
+function hacerValidaciones() {
+  for (let index = 0; index < arrayEdades.length; index++) {
+    validarEdad(document.querySelector(`#edad${index}`));
+  }
+
+  for (let index = 0; index < arraySalarios.length; index++) {
+    validarSalario(document.querySelector(`#salario${index}`));
+  }
+}
+
+// Crear elementos HTML
+
+const div = document.querySelector("div");
+
+function crearLabelEdad(divIntegrante, index) {
+  let label = document.createElement("label");
+  divIntegrante.appendChild(label).textContent = "Edad del integrante: ";
+}
+
+function crearInputEdad(divIntegrante, index) {
+  let input = document.createElement("input");
+  let inputNuevo = divIntegrante.appendChild(input);
+  inputNuevo.type = "number";
+  inputNuevo.id = `edad${index}`;
+}
+
+function crearLabelSalario(divIntegrante, index) {
+  let label = document.createElement("label");
+  divIntegrante.appendChild(label).textContent = "Salario del integrante: ";
+}
+
+function crearInputSalario(divIntegrante, index) {
+  let input = document.createElement("input");
+  let inputNuevo = divIntegrante.appendChild(input);
+  inputNuevo.type = "number";
+  inputNuevo.id = `salario${index}`;
+}
+
+function crearBr(divIntegrante, index) {
+  let br = document.createElement("br");
+  divIntegrante.appendChild(br);
+}
+
+function botonAgregarSalario(divIntegrante, index) {
+  let $botonSalario = document.createElement("button");
+  divIntegrante.appendChild($botonSalario);
+  $botonSalario.textContent = "Agregar Salario";
+  $botonSalario.onclick = () => {
+    crearLabelSalario(divIntegrante, index);
+    crearInputSalario(divIntegrante, index);
+    crearBr(divIntegrante, index);
+    crearBr(divIntegrante, index);
+
+    return false;
+  };
+}
+
+// Crear un integrante de la familia
+
+function crearIntegrante(index) {
+  let divIntegrante = document.createElement("div");
+  divIntegrante.id = `integrante${index}`;
+  div.appendChild(divIntegrante);
+
+  crearLabelEdad(divIntegrante, index);
+  crearInputEdad(divIntegrante, index);
+  botonAgregarSalario(divIntegrante, index);
+  crearBr(divIntegrante, index);
+  crearBr(divIntegrante, index);
+}
+// Crear la cantidad correspondiente a la cantidad de integrantes
+
+for (let index = 0; index < cantidadDeIntegrantes; index++) {
+  crearIntegrante(index);
+}
+// Traer los em para modificar
+
+const $resultadoEdades = document.querySelector("#resultadoEdades");
+const $resultadoSalarios = document.querySelector("#resultadoSalarios");
+
+// Calcular las edades
+
+// CREAR Y RELLENAR ARRAYS
+
+let totalEdades;
+let totalSalarios;
+const arrayEdades = [];
+const arraySalarios = [];
+
+function crearYRellenarArrays() {
+  for (let index = 0; index < cantidadDeIntegrantes; index++) {
+    let edad = Number(document.querySelector(`#edad${index}`).value);
+    console.log(edad);
+    arrayEdades.push(edad);
+
+    let salario = Number(document.querySelector(`#salario${index}`).value);
+    arraySalarios.push(salario);
+  }
+}
+totalEdades = 0;
+totalSalarios = 0;
+
+function CalcularMenorMayorPromedioYMostrar() {
+  for (let index = 0; index < arrayEdades.length; index++) {
+    totalEdades += arrayEdades[index];
+  }
+  for (let index = 0; index < arraySalarios.length; index++) {
+    totalSalarios += arraySalarios[index];
+  }
+
+  $resultadoEdades.textContent = `El promedio de edad de los integrantes es de : ${
+    totalEdades / arrayEdades.length
+  }`;
+  $resultadoSalarios.textContent = `El promedio de salario de los integrantes es de : ${
+    totalSalarios / arraySalarios.length
+  }`;
+
+  //   Buscar el mayor y el menor
+
+  //   EDADES
+
+  for (let index = 0; index < cantidadDeIntegrantes; index++) {
+    let minimoEdades = 99999999;
+    let maximoEdades = -1;
+    for (let index = 0; index < arrayEdades.length; index++) {
+      if (arrayEdades[index] < minimoEdades) {
+        minimoEdades = arrayEdades[index];
+      }
+      if (arrayEdades[index] > maximoEdades) {
+        maximoEdades = arrayEdades[index];
+      }
+    }
+    let $edadMaxima = document.querySelector("#edadMaxima");
+    $edadMaxima.textContent = `La edad maxima de la familia es: ${maximoEdades}`;
+
+    let $edadMinima = document.querySelector("#edadMinima");
+    $edadMinima.textContent = `La edad minima de la familia es: ${minimoEdades}`;
+  }
+
+  //   SALARIOS
+
+  for (let index = 0; index < cantidadDeIntegrantes; index++) {
+    let minimoSalarios = 99999999;
+    let maximoSalarios = -1;
+    for (let index = 0; index < arraySalarios.length; index++) {
+      if (arraySalarios[index] < minimoSalarios) {
+        minimoSalarios = arraySalarios[index];
+      }
+      if (arraySalarios[index] > maximoSalarios) {
+        maximoSalarios = arraySalarios[index];
+      }
+    }
+    let $edadMaxima = document.querySelector("#salarioMaximo");
+    $edadMaxima.textContent = `El salario maximo de la familia es: ${maximoSalarios}`;
+
+    let $edadMinima = document.querySelector("#salarioMinimo");
+    $edadMinima.textContent = `El salario minimo de la familia es: ${minimoSalarios}`;
+  }
+}
+
+// CREAR BOTONES
+
+// Boton Calcular
+
+const $botonCalcular = document.createElement("button");
+$botonCalcular.innerText = "Calcular";
+$botonCalcular.type = "button";
+div.appendChild($botonCalcular);
+
+$botonCalcular.onclick = () => {
+  crearYRellenarArrays();
+
+  CalcularMenorMayorPromedioYMostrar();
+
+  return false;
+};
+
+// Boton Resetear
+
+const $botonResetear = document.createElement("button");
+$botonResetear.innerText = "Resetear";
+$botonResetear.type = "reset";
+div.appendChild($botonResetear);
+
+/*
+  TAREA:
+  Crear una interfaz que permita agregar ó quitar (botones agregar y quitar) inputs+labels para completar el salario anual de cada integrante de la familia que trabaje.
+  Al hacer click en "calcular", mostrar en un elemento pre-existente el mayor salario anual, menor salario anual, salario anual promedio y salario mensual promedio.
+  
+  Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como 0).
+  */
